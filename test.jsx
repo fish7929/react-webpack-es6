7eea86ff11df5bdae4e1b6ef01be4cd256ef4ef4
fish <kingfish86@163.com>
2016-11-24 11:03:23 +0800

    TEST
    TEST....

---
+++ test.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class Test extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <h1>React ToDos App</h1>
+            </div>
+        );
+    }
+
+    
+}
+
